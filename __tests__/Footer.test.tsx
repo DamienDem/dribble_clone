8e451f4ad01f3bd7b94a9a812e6e000950f3dea3
Damien Demolin <damien.demolin@atos.net>
2024-02-29 14:41:24 +0100

    fix: select parent element for footer links

--- __tests__/Footer.test.tsx
+++ __tests__/Footer.test.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import "@testing-library/jest-dom"
-import { footerLinks } from '@/constant/footerLinks'; 
-import Footer from '@/components/Footer';
-describe('Footer Component', () => {
-  test('display the logo', () => {
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { footerLinks } from "@/constant/footerLinks";
+import Footer from "@/components/Footer";
+describe("Footer Component", () => {
+  test("display the logo", () => {
     render(<Footer />);
-    const logo = screen.getByAltText('logo');
+    const logo = screen.getByAltText("logo");
     expect(logo).toBeInTheDocument();
   });
 
-  test('display the description', () => {
+  test("display the description", () => {
     render(<Footer />);
     const descriptionText = screen.getByText(
-      'Dribble is a community for developers to share project, and get hired.'
+      "Dribble is a community for developers to share project, and get hired."
     );
     expect(descriptionText).toBeInTheDocument();
   });
 
-  test('display the links with href', () => {
+  test("display the links with href", () => {
     render(<Footer />);
-    footerLinks.forEach(link => {
-      const footerLink = screen.getByText(link.title);
+    footerLinks.forEach((link) => {
+      const footerLink = screen.getByText(link.title).parentElement;
+
       expect(footerLink).toBeInTheDocument();
-      expect(footerLink).toHaveAttribute('href', link.link);
+      expect(footerLink).toHaveAttribute("href", link.link);
     });
   });
 });
