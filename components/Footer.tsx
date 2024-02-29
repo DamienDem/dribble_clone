4e0f01dae31e501f0db916cc55ff4c53c213c395
Damien Demolin <damien.demolin@atos.net>
2024-02-29 10:35:20 +0100

    feat: footer libks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
+import { footerLinks } from "@/constant/footerLinks";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => (
   <section className="flexStart footer">
@@ -10,6 +12,13 @@ const Footer = () => (
           Dribble is a community for developers to share project, and get hired.
         </p>
       </div>
+        <div className="flex flex-wrap gap-12">
+          {footerLinks.map((link) => (
+            <Link href={link.link} key={link.title}>
+              {link.title}
+            </Link>
+          ))}
+        </div>
     </div>
   </section>
 );
---
+++ constant/footerLinks.ts
@@ -0,0 +1,26 @@
+export const footerLinks = [
+    {
+      title: 'For developers',
+      link: 'for-developers',
+    },
+    {
+      title: 'Hire developers',
+      link: 'hiring'
+    },
+    {
+      title: 'Inspiration',
+      link: 'inspiration'
+    },
+    {
+      title: 'About',
+      link: 'about'
+    },
+    {
+      title: 'Careers',
+      link: 'careers'
+    },
+    {
+      title: 'Support',
+      link: 'support'
+    },
+  ];
