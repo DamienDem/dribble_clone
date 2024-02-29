ba4abbaa99a43f9a2435dea0da63faaba2a81803
Damien Demolin <damien.demolin@atos.net>
2024-02-29 14:15:53 +0100

    feat: footer unit test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,18 +7,17 @@ const Footer = () => (
     <div className="flex flex-col gap-12 w-full">
       <div className="flex items-start flex-col">
         <Image src="/Dribbble-Logo.png" width={116} height={38} alt="logo" />
-
         <p className="text-start text-sm font-normal mt-5 max-w-xs">
           Dribble is a community for developers to share project, and get hired.
         </p>
       </div>
-        <div className="flex flex-wrap gap-12">
-          {footerLinks.map((link) => (
-            <Link href={link.link} key={link.title}>
-              {link.title}
-            </Link>
-          ))}
-        </div>
+      <div className="flex flex-wrap gap-12">
+        {footerLinks.map((link) => (
+          <Link href={link.link} key={link.title}>
+            <h4 className="font-semibold">{link.title}</h4>
+          </Link>
+        ))}
+      </div>
     </div>
   </section>
 );
